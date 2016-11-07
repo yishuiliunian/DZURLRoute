a9fdff9ad22403cf583a647926dc33a454e3c2eb
stonedong <yishuiliunian@gmail.com>
2016-11-07 21:18:45 +0800

    no message

--- DZURLRoute/Classes/DZUIStackLifeCircleAction.m
+++ DZURLRoute/Classes/DZUIStackLifeCircleAction.m
@@ -40,12 +40,15 @@ DZUIStackLifeCircleAction* DZUIShareStackInstance()
     _uiStack = [NSPointerArray weakObjectsPointerArray];;
     return self;
 }
-- (void) __logUIStack
+- (void) __logUIStack:(UIViewController*)vc selector:(SEL)sel
 {
+    NSLog(@"$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
+    NSLog(@"%@ -- %@", vc, NSStringFromSelector(sel));
     for (int i = (int)_uiStack.count - 1; i >= 0; i--) {
         UIViewController* vc = [_uiStack pointerAtIndex:i];
         NSLog(@"STACK [%d] is %@", i , vc);
     }
+    NSLog(@"$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
 }
 - (void) hostController:(UIViewController *)vc viewDidAppear:(BOOL)animated
 {
@@ -55,7 +58,7 @@ DZUIStackLifeCircleAction* DZUIShareStackInstance()
     }
     [_uiStack compact];
 #ifdef DEBUG
-    [self __logUIStack];
+    [self __logUIStack:vc selector:_cmd];
 #endif
 }
 - (void) hostController:(UIViewController *)vc viewWillDisappear:(BOOL)animated
@@ -81,7 +84,7 @@ DZUIStackLifeCircleAction* DZUIShareStackInstance()
     }
     [_uiStack compact];
 #ifdef DEBUG
-    [self __logUIStack];
+    [self __logUIStack:vc selector:_cmd];
 #endif
 }
 
